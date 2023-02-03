e3bb7cfe74a3f9707923cf59c840a07e160fa1be
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-02-03 15:22:46 +0100

    Solve syntax error

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -6,3 +6,4 @@ for server in jenkins sonarqube; do
     PUBLIC_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${server}"  --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
     sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/COMPONENT/${server}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
     aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
+done

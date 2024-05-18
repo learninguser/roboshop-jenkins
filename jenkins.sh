5adec00742b2dcf488d2ec94ed8f6d5fdb41741f
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-05-18 12:29:35 +0200

    Provide absolute path of route53.json

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ZONE_ID="Z08819113VNU2ISF56217"
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins" | jq ".Reservations[].Instances[].PublicIpAddress" |xargs)
-sed -e "s/IPADDRESS/${IP}/" route53.json >/tmp/record.json
+sed -e "s/IPADDRESS/${IP}/" /opt/roboshop-jenkins/route53.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
